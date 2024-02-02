1209afdf6898f2305a0350e57da726424af8b439
Fernando Medeiros do Nascimento <fernando_m_n@hotmail.com>
2024-02-02 16:27:24 -0300

    changing strnasitions breakpoints names to wildcards, and refactoring

--- src/app/animations/animations.ts
+++ src/app/animations/animations.ts
@@ -19,10 +19,9 @@ export const highlightedStateTrigger = trigger('highlightedState', [
 ]);
 
 export const shownStateTrigger = trigger('shownState', [
-  state('shown', style({})),
-  transition('void => shown', [
+  transition(':enter', [
     style({ opacity: 0 }),
     animate(300, style({ opacity: 1 })),
   ]),
-  transition('shown => void', [animate(300, style({ opacity: 0 }))]),
+  transition(':leave', [animate(300, style({ opacity: 0 }))]),
 ]);
